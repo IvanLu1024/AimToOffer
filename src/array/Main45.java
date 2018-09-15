a78cdcc8895751a25aec1fc74e5415ebc1143ac1
Ivan Lu <501275190@qq.com>
2018-09-15 15:30:38 +0800

    45.把数组排成最小的数

---
+++ src/array/Main45.java
@@ -0,0 +1,52 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 题目：把数组排成最小的数
+ *
+ * 描述：
+ *      输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+ *
+ *      例如输入数组 {3，32，321}，则打印出这三个数字能排成的最小数字为 321323。
+ *
+ */
+
+public class Main45 {
+
+    /**
+     * 可以看成是一个排序问题，在比较两个字符串 S1 和 S2 的大小时，
+     * 应该比较的是 S1+S2 和 S2+S1 的大小，如果 S1+S2 < S2+S1，那么应该把 S1 排在前面，
+     * 否则应该把 S2 排在前面。
+     * @param numbers
+     * @return
+     */
+    public String PrintMinNumber(int [] numbers) {
+
+        String[] str=new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<numbers.length;i++){
+            str[i]=String.valueOf(numbers[i]);
+
+        }
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                String s12 = s1 + s2;
+                String s21 = s2 + s1;
+                return s12.compareTo(s21);
+            }
+        });
+        for (String s:str){
+            sb.append(s);
+
+        }
+        return sb.toString();
+
+
+
+    }
+
+
+}
